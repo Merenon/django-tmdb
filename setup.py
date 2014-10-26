8b7d16186b9579f0cbad28ce9e2f140e711d3e6e
Dennis Dety <dennis@dety.eu>
2014-10-26 17:47:44 +0100

    Fix MANIFEST and setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
@@ -13,7 +10,6 @@ setup(
     include_package_data=True,
     license='BEER-WARE License',
     description='A small TMDB (themoviedatabase) API v3 wrapper for Django.',
-    long_description=README,
     url='https://github.com/Merenon/django-tmdb',
     author='Dennis Detering',
     author_email='dennis@dety.eu',
