1225632efe8562606d017516597fbfa8f4787db2
Dennis Dety <dennis@dety.eu>
2014-10-26 17:35:07 +0100

    Add MANIFEST file. Fix setup.py license

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BEER-WARE License',
+        'License :: Freeware',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
